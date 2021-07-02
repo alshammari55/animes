8c3feda3a6995b5a073a0d9eb0ab812b3d87293b
alshammari <>
2021-07-02 16:27:04 +0000

    new update

---
+++ aniimes.js
@@ -0,0 +1,11 @@
+alert ("welcome to my web page")
+var input = prompt ("do you like animes ?");
+console.log (input) ; 
+document.write ("ok lets begin" )
+while(input != "yes")
+(input = prompt("please answer with yes"))
+var numof = prompt ("how many pics would you like to see ?" )
+
+for(var i=0; i< numof ; i++)
+document.write ("<img src=https://th.bing.com/th/id/R.1a339fd46b9cde77f8658fa5570648cf?rik=kA%2bQefcpMI7Jow&pid=ImgRaw>"
+)
