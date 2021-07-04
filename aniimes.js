bb5c7bcc1c2a58f6a8e9a718f2e7fc2fa6b1694c
alshammari <>
2021-07-04 13:35:56 +0000

    i put a function

--- aniimes.js
+++ aniimes.js
@@ -1,5 +1,4 @@
 alert ("welcome to my web page")
-
 var input = prompt ("do you like animes ?");
 console.log (input) ; 
 document.write ("ok lets begin" )
@@ -10,7 +9,3 @@ var numof = prompt ("how many pics would you like to see ?" )
 for(var i=0; i< numof ; i++)
 document.write ("<img src=https://th.bing.com/th/id/R.1a339fd46b9cde77f8658fa5570648cf?rik=kA%2bQefcpMI7Jow&pid=ImgRaw>"
 )
- 
-
-
-
--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 alert ("welcome to my web page")
-
+function animesfunction () {
 var input = prompt ("do you like animes ?");
 console.log (input) ; 
 document.write ("ok lets begin" )
@@ -10,7 +10,6 @@ var numof = prompt ("how many pics would you like to see ?" )
 for(var i=0; i< numof ; i++)
 document.write ("<img src=https://th.bing.com/th/id/R.1a339fd46b9cde77f8658fa5570648cf?rik=kA%2bQefcpMI7Jow&pid=ImgRaw>"
 )
- 
+} 
 
-
-
+animesfunction ();
